97884f7c030c6181f1ae9f131adccf57ae42994b
张海龙 <381266395@qq.com>
2018-01-15 13:07:44 +0800

    注释掉MainActivity 的权限申请

--- app/src/main/java/com/example/jason/examination/activity/MainActivity.java
+++ app/src/main/java/com/example/jason/examination/activity/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.jason.examination.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -12,16 +11,9 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.blankj.utilcode.constant.PermissionConstants;
-import com.blankj.utilcode.util.LogUtils;
-import com.blankj.utilcode.util.PermissionUtils;
-import com.blankj.utilcode.util.ScreenUtils;
 import com.example.jason.examination.R;
 import com.example.jason.examination.base.BaseActivity;
-import com.example.jason.examination.utils.PermissionHelper;
 import com.example.jason.examination.utils.ToastHelper;
-
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -52,38 +44,38 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         initView();
-        //有权限自然会走onGranted 没权限会走 onDenied
-        PermissionUtils.permission(PermissionConstants.CAMERA, PermissionConstants.PHONE)
-                .rationale(new PermissionUtils.OnRationaleListener() {
-                    @Override
-                    public void rationale(final ShouldRequest shouldRequest) {
-                        PermissionHelper.showRationaleDialog(shouldRequest);
-                    }
-                })
-                .callback(new PermissionUtils.FullCallback() {
-                    @Override
-                    public void onGranted(List<String> permissionsGranted) {
-                        LogUtils.d("有权限自然会走onGranted");
-                    }
-
-                    @Override
-                    public void onDenied(List<String> permissionsDeniedForever,
-                                         List<String> permissionsDenied) {
-                        if (!permissionsDeniedForever.isEmpty()) {
-                            PermissionHelper.showOpenAppSettingDialog();
-                            LogUtils.d("permissionsDeniedForever.isEmpty()  没权限会走 onDenied");
-                        }
-                        LogUtils.d(permissionsDeniedForever, permissionsDenied);
-                    }
-                })
-                .theme(new PermissionUtils.ThemeCallback() {
-                    @Override
-                    public void onActivityCreate(Activity activity) {
-                        ScreenUtils.setFullScreen(activity);// 设置全屏
-                        LogUtils.d("onActivityCreate");
-                    }
-                })
-                .request();
+//        //有权限自然会走onGranted 没权限会走 onDenied
+//        PermissionUtils.permission(PermissionConstants.CAMERA, PermissionConstants.PHONE)
+//                .rationale(new PermissionUtils.OnRationaleListener() {
+//                    @Override
+//                    public void rationale(final ShouldRequest shouldRequest) {
+//                        PermissionHelper.showRationaleDialog(shouldRequest);
+//                    }
+//                })
+//                .callback(new PermissionUtils.FullCallback() {
+//                    @Override
+//                    public void onGranted(List<String> permissionsGranted) {
+//                        LogUtils.d("有权限自然会走onGranted");
+//                    }
+//
+//                    @Override
+//                    public void onDenied(List<String> permissionsDeniedForever,
+//                                         List<String> permissionsDenied) {
+//                        if (!permissionsDeniedForever.isEmpty()) {
+//                            PermissionHelper.showOpenAppSettingDialog();
+//                            LogUtils.d("permissionsDeniedForever.isEmpty()  没权限会走 onDenied");
+//                        }
+//                        LogUtils.d(permissionsDeniedForever, permissionsDenied);
+//                    }
+//                })
+//                .theme(new PermissionUtils.ThemeCallback() {
+//                    @Override
+//                    public void onActivityCreate(Activity activity) {
+//                        ScreenUtils.setFullScreen(activity);// 设置全屏
+//                        LogUtils.d("onActivityCreate");
+//                    }
+//                })
+//                .request();
     }
 
     private void initView() {
